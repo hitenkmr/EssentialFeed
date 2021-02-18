6e7ac0082e8b1701afffdac16960799092945da5
Hitender Kumar <hitender.hk@gmail.com>
2021-02-18 18:59:41 +0530

    Remove the generic error type from 'RemoteFeedResult' to simplify type constraints

--- EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedLoaderTests.swift
@@ -56,7 +56,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        expect(sut, toCompleteWithResult:.failure([.connectivity])) {
+        expect(sut, toCompleteWith:.failure(RemoteFeedLoader.Error.connectivity)) {
             let clientError = NSError(domain: "Test", code: 0, userInfo: nil)
             client.complete(with: clientError, index: 0)
         }
@@ -66,7 +66,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         let samples = [199, 201, 300, 400, 500]
         samples.enumerated().forEach { (index, code) in
-            expect(sut, toCompleteWithResult:.failure([.invalidData])) {
+            expect(sut, toCompleteWith:.failure(RemoteFeedLoader.Error.invalidData)) {
                 let data = makeJsonData(itemsJson: [])
                 client.complete(withStatusCode: code, data : data, at: index)
             }
@@ -75,7 +75,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversInvalidDataOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
-        expect(sut, toCompleteWithResult:.failure([.invalidData])) {
+        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
             let invalidData : Data = "".data(using: .utf8)!
             client.complete(withStatusCode: 200, data: invalidData, at: 0)
         }
@@ -83,7 +83,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversNoItemsOn200ResponseCodeWithEmptyListJSON() {
         let (sut, client) = makeSUT()
-        expect(sut, toCompleteWithResult: .success([])) {
+        expect(sut, toCompleteWith: .success([])) {
             let emptyJson = "{\"items\":[]}".data(using: .utf8)!
             client.complete(withStatusCode: 200, data: emptyJson, at: 0)
         }
@@ -93,7 +93,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         let item1 = makeFeedItem(id: UUID(), description: "a desc", location: "a location", imageUrl: URL(string: "http://some-a.com")!)
         let item2 = makeFeedItem(id: UUID(), description: "b desc", location: "b location", imageUrl: URL(string: "http://some-b.com")!)
-        expect(sut, toCompleteWithResult: .success([item1.modal, item2.modal])) {
+        expect(sut, toCompleteWith: .success([item1.modal, item2.modal])) {
             let data = makeJsonData(itemsJson: [item1.json, item2.json])
             client.complete(withStatusCode: 200, data: data, at: 0)
         }
@@ -128,14 +128,16 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
-    private func expect(_ sut : RemoteFeedLoader, toCompleteWithResult expectedResult : RemoteFeedLoader.Result, when action : () -> Void, message : String = "Test failed", file: StaticString = #filePath, line: UInt = #line) {
+    private func expect(_ sut : RemoteFeedLoader, toCompleteWith expectedResult : RemoteFeedLoader.Result, when action : () -> Void, message : String = "Test failed", file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "wait for load completion")
         sut.load(completion: { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedItems), .success(expectedItems)):
                 XCTAssertEqual(receivedItems, expectedItems, message, file: file, line: line)
-            case let (.failure(receivedError), .failure(expectedError)):
+                
+            case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error)):
                 XCTAssertEqual(receivedError, expectedError, message, file: file, line: line)
+                
             default:
                 XCTFail("Expected result \(expectedResult) got \(receivedResult) instead.", file: file, line: line)
             }
