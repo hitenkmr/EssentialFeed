a493123835120cfaeea55910340c637b0a01f0d9
Hitender Kumar <hitender.hk@gmail.com>
2021-02-22 10:56:55 +0530

    Enable 'Random test execution' order to guarantee that is no dependency between test methods

---
+++ EssentialFeed/EssentialFeed/Notes.swift
@@ -0,0 +1,9 @@
+//
+//  Notes.swift
+//  EssentialFeed
+//
+//  Created by Hitender Kumar on 22/02/21.
+//
+
+//Enable 'Randomize execution Order' in edit schemes for Test target
+//By randomizing our test we eliminate any possible order dependency that might exist in out tests that could affect our test results
