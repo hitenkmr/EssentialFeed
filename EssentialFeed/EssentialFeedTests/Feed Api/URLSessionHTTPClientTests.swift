b61601bc0d1bddf1d47713afd47d076d6dca4554
Hitender Kumar <hitender.hk@gmail.com>
2021-03-06 13:53:39 +0530

    Some changes in yml and removed some files

--- EssentialFeed/EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -113,7 +113,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 2.0)
         return receivedResult
     }
     
