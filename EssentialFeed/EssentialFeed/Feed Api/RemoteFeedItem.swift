4225ed4e03019049aae8dc14a25cc4d56cb6b44c
Hitender Kumar <hitender.hk@gmail.com>
2021-03-09 16:29:30 +0530

    Add comment for DTO to make its use clear

--- EssentialFeed/EssentialFeed/Feed Api/RemoteFeedItem.swift
+++ EssentialFeed/EssentialFeed/Feed Api/RemoteFeedItem.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// Instead of always passing models across boundaries, consider using a data transfer representation (also known as data transfer objects or DTO).
+
 internal struct RemoteFeedItem : Decodable {
     internal var id : UUID
     internal var description : String?
