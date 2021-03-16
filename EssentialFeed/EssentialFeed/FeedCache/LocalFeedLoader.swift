177865371e2b6c20ea49f1f494098b29a72ac90b
Hitender Kumar <hitender.hk@gmail.com>
2021-03-16 11:17:21 +0530

    Make `LocalFeedLoader` confirm to `FeedLoader` protocol

--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -51,7 +51,7 @@ extension LocalFeedLoader {
     }
 }
 
-extension LocalFeedLoader {
+extension LocalFeedLoader: FeedLoader {
     
     public func load(completion: @escaping (_ result : LoadResult) -> Void) {
         self.store.retrieve { [weak self] cacheResult in
