9edbe4c0fc4c3fb06a3736cdb27e5bcf2a354b64
Hitender Kumar <hitender.hk@gmail.com>
2021-03-17 18:28:37 +0530

    Lecture learning notes

--- EssentialFeed/EssentialFeed/Learning Outcomes.swift
+++ EssentialFeed/EssentialFeed/Learning Outcomes.swift
@@ -9,7 +9,7 @@ import Foundation
 
 //Persistence Module
 
-//lecture covered - 'Separating Queries & Side-effects for Simplicity and Reusability, Choosing Between Enum Switching Strategies, and Differentiating App-Specific from App-Agnostic Logic' https://academy.essentialdeveloper.com/courses/447455/lectures/9677292
+//1. lecture covered - 'Separating Queries & Side-effects for Simplicity and Reusability, Choosing Between Enum Switching Strategies, and Differentiating App-Specific from App-Agnostic Logic' https://academy.essentialdeveloper.com/courses/447455/lectures/9677292
 
 /*Command–Query Separation (CQS) principle - is a programming principle that can help identify function/method that do too much
  
@@ -27,6 +27,27 @@ import Foundation
  - For example, we can schedule cache validation every 10 minutes or every time the app goes to (or gets back from) the background (instead of only performing it when the user requests to see the feed).
  */
 
+//2. Separating App-specific, App-agnostic & Framework logic, Entities vs. Value Objects, Establishing Single Sources of Truth, and Designing Side-effect-free (Deterministic) Domain Models with Functional Core, Imperative Shell Principles
+//https://academy.essentialdeveloper.com/courses/447455/lectures/9763057
+
+/* - Application-specific vs. Application-agnostic vs. Framework (Infrastructure) Logic
+ - Entities vs. Value objects
+ 
+ - Designing side-effect free (deterministic) core business rules
+ 
+ - Establishing Functional Core, Imperative Shell
+ 
+ - Promoting reusability and reducing cost, duplication, and defects with single sources of truth*/
 
 
+//3. https://academy.essentialdeveloper.com/courses/447455/lectures/9763057/comments/10738454
+
+/*
+ - The anatomy of Dependency Inversion (High-level, Low-level, and Boundary components)
+ Specs as Contracts
+ 
+ - Proactively avoiding bugs caused by side-effects in multithreaded environments
+ 
+ - Documenting infrastructure requirements with an Inbox checklist
+ */
 
