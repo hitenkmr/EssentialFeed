9eba49f3e83dc3d4b24595ba43bd156fc717f8c8
Hitender Kumar <hitender.hk@gmail.com>
2021-04-11 15:17:12 +0530

    Make 'CodableFeedStore' confirm to 'FeedStore'

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import EssentialFeed
 
-class CodableFeedStore {
+class CodableFeedStore: FeedStore {
     
     private  struct Cache: Codable {
         let feed: [CodableFeedImage]
@@ -37,7 +37,7 @@ class CodableFeedStore {
         }
     }
     
-    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+    func retrieve(completion: @escaping RetrievalCompletion) {
         guard let data = try? Data(contentsOf: storeURL) else {
             return completion(.empty)
         }
@@ -52,7 +52,7 @@ class CodableFeedStore {
         }
     }
     
-    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         guard FileManager.default.fileExists(atPath: storeURL.path) else {
             return completion(nil)
         }
@@ -71,7 +71,7 @@ class CodableFeedStore {
         self.storeURL = storeURL
     }
     
-    func insert(feed: [LocalFeedImage] , timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+    func insert(feed: [LocalFeedImage] , timestamp: Date, completion: @escaping InsertionCompletion) {
         
         do {
             let encoder = JSONEncoder()
