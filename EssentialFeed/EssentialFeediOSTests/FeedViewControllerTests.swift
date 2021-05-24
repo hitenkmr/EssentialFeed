c39547bb859d6fe8d58e3cd94f4e9b954878514a
Hitender Kumar <hitender.hk@gmail.com>
2021-05-24 09:00:20 +0530

    Extract UIImage test helper to a separate file

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -426,19 +426,6 @@ private extension FeedImageCell {
     }
 }
 
-private extension UIImage {
-    //creates a tiny image without overhead of hitting the disk so that tests can be run fast
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
-    }
-}
 
 private extension UIButton {
     func simulateTap() {
