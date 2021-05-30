d7cd801ff38517d2b2751722a46e496899f4feef
Hitender Kumar <hitender.hk@gmail.com>
2021-05-30 11:59:45 +0530

    Decouple FeedPresenter from FeedLoader with an adapter in the Composition layer

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -30,22 +30,19 @@ final class FeedPresenter {
     
     typealias Observer<T> = (T) -> Void
     
-    private let feedLoader: FeedLoader
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
     var feedView: FeedView?
     var loadingView: FeedLoadingView?
     
-    func loadFeed() {
-        self.loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: true))
-        feedLoader.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.feedView?.display(viewModel: FeedViewModel(feed: feed))
-            }
-            self?.loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: false))
-        }
+    func didStartLoadingFeed() {
+        loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: true))
+    }
+    
+    func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView?.display(viewModel: FeedViewModel(feed: feed))
+        loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: false))
+    }
+    
+    func didFinishLoadingFeed(with error: Error) {
+        loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: false))
     }
 }
