713498c77c1d67b62e830e07d63bda0846256c92
Hitender Kumar <hitender.hk@gmail.com>
2021-06-05 07:56:14 +0530

    RemoteFeedImageDataLoader.loadImageDataFromURL delivers connectivity error on HTTP client error

--- EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedImageDataLoaderTests.swift
@@ -30,10 +30,10 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
-    func test_loadImageDataFromURL_deliversErrorOnClientError() {
+    func test_loadImageDataFromURL_deliversConnectivityErrorOnClientError() {
         let (sut,client) = makeSUT()
         let clientError = anyNSError()
-        expect(sut, toCompleteWith: .failure(clientError)) {
+        expect(sut, toCompleteWith: failure(.connectivity)) {
             client.complete(with: clientError)
         }
     }
