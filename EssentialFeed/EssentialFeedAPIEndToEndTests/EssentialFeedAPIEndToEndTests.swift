70db97cb3bd9cda6e6b08c00d8da0c212d9dfea4
Hitender Kumar <hitender.hk@gmail.com>
2021-06-05 08:23:47 +0530

    Extract duplicate HTTPURLResponse.statusCode == 200 validation into a `isOK` helper method

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -74,7 +74,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 10.0)
+        wait(for: [exp], timeout: 5.0)
         return receivedResult
     }
     
