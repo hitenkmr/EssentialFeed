71464485a9c3547aeec0539708db70b832ad051b
Hitender Kumar <hitender.hk@gmail.com>
2021-06-08 18:29:06 +0530

    Separate retrieval from insertion operations and types to clarify intent

--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedImageDataLoader.swift
@@ -7,15 +7,26 @@
 
 import Foundation
 
-public final class LocalFeedImageDataLoader: FeedImageDataLoader {
+public final class LocalFeedImageDataLoader {
     
     private let store: FeedImageDataStore
-
+    
     public init(store: FeedImageDataStore) {
         self.store = store
     }
-    
-    private final class Task: FeedImageDataLoaderTask {
+}
+
+extension LocalFeedImageDataLoader {
+    public typealias SaveResult = Result<Void, Error>
+
+    public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
+        store.insert(data, for: url) { _ in }
+    }
+}
+
+extension LocalFeedImageDataLoader: FeedImageDataLoader {
+     
+    private final class LoadImageDataTask: FeedImageDataLoaderTask {
         private var completion: ((FeedImageDataLoader.Result) -> Void)?
         
         init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
@@ -35,25 +46,19 @@ public final class LocalFeedImageDataLoader: FeedImageDataLoader {
         }
     }
     
-    public enum Error: Swift.Error {
+    public enum LoadError: Swift.Error {
         case failed
         case notFound
     }
-        
-    public typealias SaveResult = Result<Void, Swift.Error>
-
-    public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
-        store.insert(data, for: url) { _ in }
-    }
     
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = Task(completion)
+        let task = LoadImageDataTask(completion)
         self.store.retrieve(dataForURL: url, completion: { [weak self] result in
             guard self != nil else { return }
             task.complete(with: result
-                            .mapError { _ in Error.failed }
+                            .mapError { _ in LoadError.failed }
                             .flatMap { data in
-                                data.map { .success($0) } ?? .failure(Error.notFound)
+                                data.map { .success($0) } ?? .failure(LoadError.notFound)
                             })
         })
         return task
