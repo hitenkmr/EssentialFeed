5a6477b5bc183f21de6d48f1d5f2ce3dd9f6f7a8
Hitender Kumar <hitender.hk@gmail.com>
2021-06-20 16:35:03 +0530

    Record feed with content snapshot

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedImageViewModel.swift
@@ -9,13 +9,22 @@ import Foundation
 import EssentialFeed
 
 public struct FeedImageViewModel<Image> {
-    let description: String?
-    let location: String?
-    let image: Image?
-    let isLoading: Bool
-    let shouldRetry: Bool
     
-    var hasLocation: Bool {
+    public init(description: String?, location: String?, image: Image?, isLoading: Bool, shouldRetry: Bool) {
+        self.description = description
+        self.location = location
+        self.image = image
+        self.isLoading = isLoading
+        self.shouldRetry = shouldRetry
+    }
+    
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
         return location != nil
     }
 }
