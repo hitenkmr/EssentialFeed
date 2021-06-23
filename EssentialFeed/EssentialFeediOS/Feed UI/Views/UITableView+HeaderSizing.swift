f8781c52e4d37816b3136d94f313ea1e5e80f587
Hitender Kumar <hitender.hk@gmail.com>
2021-06-24 00:00:41 +0530

    resize table header

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/UITableView+HeaderSizing.swift
@@ -0,0 +1,22 @@
+//
+//  UITableView+HeaderSizing.swift
+//  EssentialFeediOS
+//
+//  Created by Hitender Kumar on 20/06/21.
+//
+
+import UIKit
+
+extension UITableView {
+    func sizeTableHeaderToFit() {
+        guard let header = tableHeaderView else { return }
+
+        let size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+
+        let needsFrameUpdate = header.frame.height != size.height
+        if needsFrameUpdate {
+            header.frame.size.height = size.height
+            tableHeaderView = header
+        }
+    }
+}
