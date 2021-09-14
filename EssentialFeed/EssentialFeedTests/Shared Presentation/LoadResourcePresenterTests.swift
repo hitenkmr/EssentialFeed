0e5b7c127a0cb9a23b6a980b4be4c7a2926a133a
Hitender kumar <hitender.hk@gmail.com>
2021-09-14 15:46:54 +0530

    rename methods

--- EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -16,7 +16,7 @@ class LoadResourcePresenterTests: XCTestCase {
     func test_didStartLoadingFeed_displaysNoErrorMessageAndStartsLoading() {
         let (sut, view) = makeSUT()
 
-        sut.didStartLoadingFeed()
+        sut.didStartLoading()
 
         XCTAssertEqual(view.messages, [
             .display(errorMessage: .none),
@@ -28,7 +28,7 @@ class LoadResourcePresenterTests: XCTestCase {
         let (sut, view) = makeSUT()
         let feed = uniqueImageFeed().models
         
-        sut.didFinishLoadingFeed(with: feed)
+        sut.didFinishLoading(with: feed)
         
         XCTAssertEqual(view.messages, [
             .display(feed: feed),
@@ -39,7 +39,7 @@ class LoadResourcePresenterTests: XCTestCase {
     func test_didFinishLoadingFeedWithError_displaysLocalizedErrorMessageAndStopsLoading() {
         let (sut, view) = makeSUT()
         
-        sut.didFinishLoadingFeed(with: anyNSError())
+        sut.didFinishLoading(with: anyNSError())
         
         XCTAssertEqual(view.messages, [
             .display(errorMessage: localized("FEED_VIEW_CONNECTION_ERROR")),
