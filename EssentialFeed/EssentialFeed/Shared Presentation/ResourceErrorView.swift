ecdd472b10a88966118d874795968edbc2867118
Hitender kumar <hitender.hk@gmail.com>
2021-09-15 15:20:37 +0530

    Rename and move ResourceErrorView and ViewModel to Shared module

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,9 @@
+//	
+//  Copyright © 2021 Hitender Kumar. All rights reserved.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceErrorViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2021 Hitender Kumar. All rights reserved.
+//
+
+public struct ResourceErrorViewModel {
+	public let message: String?
+	
+	static var noError: ResourceErrorViewModel {
+		return ResourceErrorViewModel(message: nil)
+	}
+	
+	static func error(message: String) -> ResourceErrorViewModel {
+		return ResourceErrorViewModel(message: message)
+	}
+}
